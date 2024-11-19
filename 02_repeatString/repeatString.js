188fb0d1cc7b44aa01859285432b558d9cff40c9
Alex <114047696+bigspaceworm@users.noreply.github.com>
2024-11-19 16:32:52 -0600

    Solve exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+	let stringRepeated = "";
+	if(number < 0) {
+		stringRepeated = "ERROR";
+	}
+	else {
+		for(let i = 0; i < number; i++) {
+			stringRepeated += string;
+		}
+	}
 
+	return stringRepeated;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	let reversedWord = "";
+	for(let i = word.length - 1; i >= 0; i--) {
+		reversedWord += word.charAt(i);
+	}
 
+	return reversedWord;
 };
 
 // Do not edit below this line
