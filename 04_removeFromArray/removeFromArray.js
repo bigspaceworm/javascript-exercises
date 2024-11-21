eba52e19847dc53aed7d1bd5514577ee64cc7aa3
Alex <114047696+bigspaceworm@users.noreply.github.com>
2024-11-21 15:19:26 -0600

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(startingArray, ...valueToDelete) {
+	let checkIfReturns = (valToCheck) => {
+		let hasCoincidences = 0;
+		for(let i = 0; i < valueToDelete.length; i++) {
+			if(valToCheck === valueToDelete[i]){
+				hasCoincidences += 1;
+			}
+		}
+
+		return hasCoincidences === 0 ? valToCheck : null;
+	}
+
+	return startingArray.filter(checkIfReturns);
 };
 
 // Do not edit below this line
