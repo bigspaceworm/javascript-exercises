e56df059452137239d85bae5daa7ae7e90c48d28
Alex <114047696+bigspaceworm@users.noreply.github.com>
2024-11-21 17:42:47 -0600

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+	let sum = 0;
+
+	if(Number.isInteger(first) && Number.isInteger(second) 
+	&& first > 0 && second > 0) {
+		if(first > second){
+			let temp = first;
+			first = second;
+			second = temp;
+		}
+
+		for(i = first; i <= second; i++) {
+			sum += i;
+		}
+
+		return sum;
+	}
+	else {
+		return "ERROR";
+	}
 
 };
 
